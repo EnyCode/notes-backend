8ec4f5696262beaafd51a1c9b9bb35845327f127
EnyCode <69588272+EnyCode@users.noreply.github.com>
2024-07-26 16:16:30 +0100

    get basic working

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ extern crate rocket;
 use std::str::FromStr;
 
 use crate::rocket::tokio::io::AsyncReadExt;
+use regex::Regex;
 use rocket::fs::TempFile;
-use rtf_parser::{document::RtfDocument, lexer::Lexer, tokens::Token};
+use rtf_parser::{document::RtfDocument, lexer::Lexer, parser::StyleBlock, tokens::Token};
 
 #[get("/")]
 async fn index() -> &'static str {
@@ -13,7 +14,9 @@ async fn index() -> &'static str {
 
 #[post("/upload/<note>", data = "<file>")]
 async fn upload(note: &str, file: TempFile<'_>) -> String {
+    let re = Regex::new(r"\\AppleTypeServices(?:.+? )").unwrap();
     let mut rst = String::new();
+
     file.open()
         .await
         .unwrap()
@@ -21,34 +24,57 @@ async fn upload(note: &str, file: TempFile<'_>) -> String {
         .await
         .unwrap();
 
-    let tokens: Vec<Token> = Lexer::scan(&rst).unwrap();
+    rst = (*re.replace_all(&rst, "")).to_string();
 
-    //println!("{:#?}", tokens);
-    doc_to_md(tokens).await;
+    let doc = match RtfDocument::try_from(rst) {
+        Ok(doc) => doc,
+        Err(_) => return String::from_str("Error parsing RTF").unwrap(),
+    };
+
+    println!("{:#?}", doc.get_text());
+    println!("{}", doc_to_md(doc).await);
 
     return format!("{:#?}", "hey");
 }
 
-struct Paragraph {
-    text: String,
-}
+fn token_to_md(token: &StyleBlock) -> String {
+    let trimmed_end = token.text.trim_end();
+    let trimmed_start = token.text.trim_start();
+    let trimmed = token.text.trim();
+    let text = &token.text;
 
-async fn doc_to_md(tokens: Vec<Token<'_>>) -> String {
-    let md = String::new();
+    let start = &text[..(text.len() - trimmed_start.len())];
+    let end = &text[trimmed_end.len()..text.len()];
 
-    let mut stack = Vec::new();
+    let mut prefixsuffix = String::new();
 
-    for token in tokens {
-        match token {
-            Token::CRLF => {
-                println!("{:#?}", stack);
-                stack.clear();
-            }
-            _ => stack.push(token),
-        }
+    if token.painter.bold {
+        prefixsuffix.push_str("**");
+    }
+    if token.painter.italic {
+        prefixsuffix.push_str("*");
     }
 
-    return String::from_str("Hello").unwrap();
+    println!(":{}: :{}: and {} ({})", start, end, prefixsuffix, trimmed);
+
+    format!(
+        "{}{}{}{}{}",
+        start, prefixsuffix, trimmed, prefixsuffix, end
+    )
+}
+
+async fn doc_to_md(doc: RtfDocument) -> String {
+    let mut md = String::new();
+
+    let mut i = 0;
+    let len = doc.body.len();
+    while i < len {
+        let b = &doc.body[i];
+        i += 1;
+        md.push_str(&token_to_md(b));
+    }
+
+    md
 }
 
 #[launch]
